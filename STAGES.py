b3e6db3de55a5d068ef33ff43bda3baf88b70fcb
Lucas Sobral <lucas.sobral@itixti.com.br>
2021-05-20 18:06:31 -0300

    Adição em to_sql(index=False)

--- STAGES.py
+++ STAGES.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def create_stg_tables_from_csv(path_location, delimiter, conn_output, coding, table_name):
-    df = pd.read_csv(path_location, sep=delimiter, low_memory=False,
-                                encoding=coding).to_sql(name=table_name, con=conn_output,
-                                    schema='STAGED', if_exists='replace', chunksize=100)
+    df = pd.read_csv(path_location, sep=delimiter, low_memory=False, encoding=coding,
+                     index = False).to_sql(name=table_name,con=conn_output,
+                                           schema='STAGED', if_exists='replace', chunksize=100)
