4d48a190daffecf1db7b76d0a478f710e75c20f9
Lucas Torres Sobral <sobrallucas15@gmail.com>
2021-05-31 13:47:06 -0300

    Log de execução

--- DAG_MAIN.py
+++ DAG_MAIN.py
@@ -23,16 +23,64 @@ start_time = time.time()
 print(f"Tempo total D_LOCALIDADE: {D_LOCALIDADE.run_dim_localidade(connection_postgre):.4f}",
       "\n\n---------------------------------------------------------------------------\n")
 
-print(f"Tempo total D_ESTOLA: {D_ESCOLA.run_dim_escola(connection_postgre):.4f}",
+print(f"Tempo total D_ESCOLA: {D_ESCOLA.run_dim_escola(connection_postgre):.4f}",
       "\n\n---------------------------------------------------------------------------\n")
 
 print(f"Tempo total D_TURMA: {D_TURMA.run_dim_turma(connection_postgre):.4f}",
       "\n\n---------------------------------------------------------------------------\n")
 
-print(f"Tempo total F_APLICACAO: "
+print(f"Tempo total F_Aplicação_Questionário: "
     f"{F_APLICACAO_QUESTIONARIO.run_fact_aplicacao_questionario(connection_postgre):.4f}",
     "\n\n-----------------------------------------------------------------------------\n")
 
-
 end_time = time.time()
 print(f"\nBanco atualizado em : {end_time - start_time:.2f} segundos")
+
+
+"""
+LOG-Saída
+
+D_LOCALIDADE
+extract: 0.082
+treat: 0.029
+load: 0.636
+Tempo total D_LOCALIDADE: 0.7478 
+
+---------------------------------------------------------------------------
+
+D_ESCOLA
+extract: 1.737
+treat: 0.317
+Load da Dimensão Escola: 
+
+100%|██████████| 20/20 [00:12<00:00,  1.62it/s]
+load: 12.420
+Tempo total D_ESCOLA: 14.4743 
+
+---------------------------------------------------------------------------
+
+D_TURMA
+extract: 29.460
+treat: 7.004
+Load da Dimensão Turma: 
+
+100%|██████████| 50/50 [04:24<00:00,  5.28s/it]
+load: 264.627
+Tempo total D_TURMA: 301.0906 
+
+---------------------------------------------------------------------------
+
+F_Aplicação_Questionário
+extract: 102.540
+treat: 43.499
+Load da Fato Questionário: 
+
+100%|██████████| 50/50 [21:26<00:00, 25.73s/it]
+load: 1287.596
+Tempo total F_Aplicação_Questionário: 1433.6343 
+
+-----------------------------------------------------------------------------
+
+Banco atualizado em : 1765.74 segundos
+
+"""
